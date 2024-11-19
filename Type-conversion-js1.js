2e88beed34decdfb047c47d6ed7dc60c8416b342
Engy2228 <engy2228@gmail.com>
2024-11-19 12:20:31 +0300

    Приведение типов в JavaScript задание 1

---
+++ Type-conversion-js1.js
@@ -0,0 +1,72 @@
+/*
+Symbol
+*/
+
+let price = 0
+console.log('number price:', Number(price))
+console.log('number price:', typeof Number(price))
+console.log('string price:', String(price))
+console.log('string price:', typeof String(price))
+console.log('boolein price:', Boolean(price))
+console.log('boolein price:', typeof Boolean(price))
+
+let animal = 'girafa!'
+console.log('string animal:', String(animal))
+console.log('string animal:', typeof animal)
+console.log('number animal:', Number(animal))
+console.log('number animal:', typeof Number(animal))
+console.log('boolean animal:', Boolean(animal))
+console.log('boolean animal', typeof Boolean(animal))
+
+let word = BigInt
+console.log('boolean word:', Boolean(word))
+console.log('boolean word:', typeof Boolean(word))
+console.log('string word:', String(word))
+console.log('string word', typeof String(word))
+console.log('number word:', Number(word))
+console.log('number word:', typeof Number(word))
+
+let count = null
+console.log('boolean count', Boolean(count))
+console.log('boolean count', typeof Boolean(count))
+console.log('string count:', String(count))
+console.log('string count:', typeof String(count))
+console.log('number count:', Number(count))
+console.log('number count:', typeof Number(count))
+
+
+let counted = '0'
+console.log('boolean counted:', Boolean(counted))
+console.log('boolean counted:', typeof Boolean(counted))
+console.log('string counted:', String(counted))
+console.log('string counted:', typeof String(counted))
+console.log('number counted:', Number(counted))
+console.log('number counted:', typeof Number(counted))
+
+
+let name
+console.log('boolean name:', Boolean(name))
+console.log('boolean name:', typeof Boolean(name))
+console.log('string name:', String(name))
+console.log('string name', typeof String(name))
+console.log('number name:', Number(name))
+console.log('number name:', typeof Number(name))
+
+let agent = Object
+console.log('boolean agent:', Boolean(agent))
+console.log('boolean agent:', typeof Boolean(agent))
+console.log('string agent:', String(agent))
+console.log('string agent', typeof String(agent))
+console.log('number agent:', Number(agent))
+console.log('number agent:', typeof Number(agent))
+
+let myLive = 'Symbol(id)'
+console.log('boolean myLive:', Boolean(myLive))
+console.log('boolean myLive:', typeof Boolean(myLive))
+console.log('string myLive:', String(myLive))
+console.log('string myLive', typeof String(myLive))
+console.log('number myLive:', Number(myLive))
+console.log('number myLive:', typeof Number(myLive))
+
+
+

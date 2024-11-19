e64b1be7e98ad05aae301fa32e4b14cc53cdb570
Engy2228 <engy2228@gmail.com>
2024-11-19 12:57:50 +0300

    Введение в основы JavaScript задание 5

---
+++ BasicsJS5.js
@@ -0,0 +1,22 @@
+let i = 0
+let sum = 0
+
+do {sum += i, i += 1 
+  console.log('i / sum', i, sum)
+}
+while (i <= 100)
+  alert(sum)  
+
+
+
+let x = 0;
+let summ = 0;
+
+for (x = 0; x <= 100; x += 1) {
+  summ += x;
+console.log('i / summ', x, summ)} // Сначала добавляем текущее значение i к sum
+  alert(summ); // Используем конкатенацию для правильного вывода
+
+
+
+

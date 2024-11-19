a7a950e9a774407102057a218a0822b28311e45c
Engy2228 <engy2228@gmail.com>
2024-11-19 12:38:20 +0300

    Работа со строками задание 1

---
+++ Working-with-strings1.js
@@ -0,0 +1,23 @@
+
+
+const text1 = 'Всем привет! Меня зовут'
+const text2 = 'Сейчас я изучаю язык программирования'
+const text3 = 'на курсе по'
+const text4 = 'у'
+const text5 = 'Я хочу стать веб-разработчиком, потому что'
+const text6 = 'До этого я изучал(а)'
+const text7 = 'месяцев(а). Я уверен(а), что пройду данный курс до конца!»'
+
+const myName = 'Evgen.'
+const programmingLanguage = 'JavaScript'
+const courseCreatorName = 'Vladilen Minin.'
+const reasonText = 'I want to buy new bike.'
+const numberOfMonth = 0 
+
+
+const allInfo = `${text1} ${myName} ${text2} ${programmingLanguage} ${text3} ${programmingLanguage} ${text4} ${courseCreatorName} ${text5} ${reasonText} ${text6} ${programmingLanguage} ${numberOfMonth} ${text7}`;
+
+console.log(allInfo)
+
+const string2 ='Hello my  name   is    Ivan';
+console.log(string2.trim());

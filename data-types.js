980a0e0ad2fd81fd2d01994682aafa6486adcfa6
Engy2228 <engy2228@gmail.com>
2024-11-19 11:11:42 +0300

    Типы данных задание 1

---
+++ data-types.js
@@ -0,0 +1,28 @@
+/*Symbol */
+const myName = 'Evgen'
+console.log('myName', myName, typeof myName)
+
+const weidth = 158
+console.log('weidth', weidth, typeof weidth)
+
+const myGender = 'femail'
+console.log('myGender', myGender, typeof true)
+
+let myNewHouse;
+console.log('myNewHouse', myNewHouse, typeof underfiend)
+
+let myPreviousApartment = null
+console.log('myPreviousApartment', myPreviousApartment, typeof null)
+
+const car ={
+  favoritCar: 'Mercedes',
+  power: 200,
+  color: 'whitePerlamutr'
+}
+console.log('car', car, typeof console)
+
+const count = 10n
+console.log('count', count, typeof BigInt)
+
+const passport = Symbol('passport')
+console.log('passport', passport, typeof Symbol)

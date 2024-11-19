0ef180a72365b0119c495da9b955ba0250d52090
Engy2228 <engy2228@gmail.com>
2024-11-19 12:55:10 +0300

    Введение в основы JavaScript задание 3

---
+++ Confirm.js
@@ -0,0 +1,28 @@
+const question1 = confirm('JavaScript появился в 1995 году?')
+if (question1 === true) {
+  alert('Верно.')
+} else {
+  alert('Не верно. Первая версия языка JavaScript была разработана Бренданом Эйхом за 10 дней в 1995 году в компании Netscape.')
+
+
+}
+console.log(question1)
+
+const question2 = confirm('Спецификация JavaScript называется ECMAScript?')
+if (question2 === true) {
+  alert('Верно')
+} else {
+  alert('Не верно. Спецификация JavaScript называется ECMAScript')
+}
+console.log(question2)
+
+const question3 = confirm('JavaScript был создан за 1 месяц?')
+if (question3 === true) {
+  alert('Не верно. Первая версия языка JavaScript была разработана Бренданом Эйхом за 10 дней в 1995 году в компании Netscape.')
+
+} else {
+  alert('Верно')
+
+}
+console.log(question3)
+

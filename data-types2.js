dab7ce2ffa7beec22dada4f58624fa08b4ccc870
Engy2228 <engy2228@gmail.com>
2024-11-19 11:57:07 +0300

    Типы данных задание 2

---
+++ data-types2.js
@@ -0,0 +1,16 @@
+let ageMyFriend = 43
+ageMyFriend = 'Forty three'
+
+console.log('ageMyFriend', ageMyFriend, typeof ageMyFriend) 
+alert(ageMyFriend)
+
+let myFriendName = 'Kosta'
+myFriendName = 43
+
+console.log('myFriendName', myFriendName, typeof myFriendName)
+alert(myFriendName)
+
+let friendly = true
+friendly = null
+console.log('friendly', friendly, typeof friendly)
+alert(friendly)
